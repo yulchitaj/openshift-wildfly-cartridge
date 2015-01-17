6d9637f0a526283b76d1c95e3ffb305b3004e16c
SiryjVyiko <dz@hivext.net>
2015-01-17 14:30:53 +0200

    Update firewall.sh

--- jelastic/scripts/firewall.sh
+++ jelastic/scripts/firewall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eu
 
-/sbin/iptables -I INPUT -p tcp --dport 4949 -j ACCEPT
 /sbin/iptables -t nat -I PREROUTING -p tcp -m tcp --dport 4949 -j REDIRECT --to-ports 9990
+/sbin/iptables -A INPUT -p tcp --dport 4949 -j ACCEPT
